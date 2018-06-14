93c68b57c8c5cf096e56419fbcf9e9b84ef21079
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2018-06-14 17:30:24 +0200

    Add license and license headers

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/*
+ * Kuzzle, a backend software, self-hostable and ready to use
+ * to power modern apps
+ *
+ * Copyright 2015-2018 Kuzzle
+ * mailto: support AT kuzzle.io
+ * website: http://kuzzle.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 const
   BigQuery = require('@google-cloud/bigquery'),
   debug = require('debug')('kuzzle:kdc:bigQuery'),
@@ -133,12 +155,12 @@ class BigQueryConnector {
     this.context.log.info(`-----------------------------------------`);
     if (
       this.probes[measure.probeName] &&
-      typeof this.probes[measure.probeName].timestamp != 'undefined' && 
+      typeof this.probes[measure.probeName].timestamp != 'undefined' &&
       this.probes[measure.probeName].timestamp
     ) {
       data[0].timestamp = Math.round((new Date()).getTime() / 1000);
     }
-    
+
     debug(`Received measure from probe ${measure.probeName}`);
     debug(JSON.stringify(data));
 
@@ -261,7 +283,7 @@ function buildMonitorSchema(hooks) {
 function extractMeasureData(data) {
   if (data.content) {
     let extractedData = [flattenObject(data.content)];
-    
+
     return extractedData.map(datum => {
       return normalizeMeasureData(datum);
     });
